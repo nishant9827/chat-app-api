a674c6f229889747487c09e8f31c2bbad93fc602
nishantgupta9827 <gnishant790@gmail.com>
2024-01-15 22:05:14 +0530

    change speling

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const server = app.listen(port,()=>{
 const io = socket(server,{
     cors:{
         origin:"https://chat-app-lemon-five-15.vercel.app",
-        Credentials:true,
+        credentials:true,
     }
 })
 
