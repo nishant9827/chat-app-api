22770183eb7f0717799621ddbe122b9ba9fbeb45
nishantgupta9827 <gnishant790@gmail.com>
2024-07-15 12:02:03 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dbUrl = process.env.MONGODBURL;
 
-mongoose.connect(dbUrl);
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
-const db= mongoose.connection;
+const db = mongoose.connection;
 
-db.on('error',console.error.bind(console,'MongDb connection error'));
-db.once('open',()=>{
-    console.log('Connected to MongoDb');
-})
+db.on("error", console.error.bind(console, "MongDb connection error"));
+db.once("open", () => {
+  console.log("Connected to MongoDb");
+});
 module.exports = mongoose;
